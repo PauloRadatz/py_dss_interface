f14eabf8dd5a9bb82081b4fdfb1a60e9a30d7dd9
Paulo Radatz <paulo.radatz@gmail.com>
2020-08-18 09:25:09 -0400

    V0.0.5

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ def read(*names, **kwargs):
 
 setup(
     name='py-dss-interface',
-    version='0.0.4',
+    version='0.0.5',
     license='MIT',
     description='opendsspy is a Windows Python package providing access to OpenDSS direct dll version of OpenDSS.',
     long_description='%s\n%s' % (
