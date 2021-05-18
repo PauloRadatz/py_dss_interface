98f628185c8e3d72100326583afb4bcdfc00c50d
PauloRadatz <paulo.radatz@gmail.com>
2021-05-18 19:41:17 -0400

    adding colorama package as depend.

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
         # eg: 'keyword1', 'keyword2', 'keyword3',
     ],
     python_requires='>=3.6',
-    install_requires=["numpy", "pandas"
+    install_requires=["numpy", "pandas", "colorama"
                       # eg: 'aspectlib==1.1.1', 'six>=1.7',
                       ],
     extras_require={
