cf5a2f360c64d1946ad0dc7ae6f5be6afdde413b
eniocc <eniocc@gmail.com>
2021-06-20 15:50:31 -0300

    A elegant way to create examples was added, enjoy the ExampleBase class.

--- examples/01_Example_Bus_Class_methods.py
+++ examples/01_Example_Bus_Class_methods.py
@@ -1,18 +1,10 @@
 # -*- encoding: utf-8 -*-
 """
- Created by eniocc at 12/05/2021
+ Created by Ênio Viana at 12/05/2021
 """
-import os
-import pathlib
-from py_dss_interface import DSSDLL as DSS
+from py_dss_interface.models.example.ExampleBase import ExampleBase
 
-dss = DSS()
-
-my_path = pathlib.Path(__file__).parents[1] # one level above
-my_path = os.path.join(my_path, r"test\py_dss_interface\13Bus")
-dss_file = os.path.join(my_path, "IEEE13Nodeckt.dss")
-
-dss.text("compile {0}".format(dss_file))
+dss = ExampleBase("13").dss
 
 # Integer methods
 print(45 * '=' + ' Integer Methods ' + 45 * '=')
@@ -60,7 +52,7 @@ print(f'dss.bus_name(): {dss.bus_name()}')
 # Variant methods
 print(45 * '=' + ' Variant Methods ' + 45 * '=')
 print(f'dss.bus_voltages(): {dss.bus_voltages()}')
-print(f'dss.bus_seqvoltages(): {dss.bus_seqvoltages()}')
+print(f'dss.bus_seqvoltages(): {dss.bus_seq_voltages()}')
 print(f'dss.bus_nodes(): {dss.bus_nodes()}')
 print(f'dss.bus_voc(): {dss.bus_voc()}')
 print(f'dss.bus_isc(): {dss.bus_isc()}')
--- examples/07_Example_CtrlQueue_Class_methods.py
+++ examples/07_Example_CtrlQueue_Class_methods.py
@@ -1,18 +1,10 @@
 # -*- encoding: utf-8 -*-
 """
- Created by eniocc at 12/05/2021
+ Created by Ênio Viana at 12/05/2021
 """
-import os
-import pathlib
-from py_dss_interface import DSSDLL as DSS
+from py_dss_interface.models.example.ExampleBase import ExampleBase
 
-dss = DSS()
-
-my_path = pathlib.Path(__file__).parents[1] # one level above
-my_path = os.path.join(my_path, r"test\py_dss_interface\13Bus")
-dss_file = os.path.join(my_path, "IEEE13Nodeckt.dss")
-
-dss.text("compile {0}".format(dss_file))
+dss = ExampleBase("13").dss
 
 # Integer methods
 print(45 * '=' + ' Integer Methods' + 45 * '=')
@@ -28,12 +20,6 @@ print(f'dss.ctrlqueue_clearactions(): {dss.ctrlqueue_clear_actions()}')
 print(f'dss.ctrlqueue_popaction(): {dss.ctrlqueue_pop_action()}')
 print(f'dss.ctrlqueue_queuesize(): {dss.ctrlqueue_queue_size()}')
 print(f'dss.ctrlqueue_doallqueue(): {dss.ctrlqueue_do_all_queue()}')
-
-# To iterate from begin we must call first()
-# dss.active_class_first()
-# for i in range(dss.active_class_num_elements()):
-#     print(f'Name: {dss.active_class_get_name()} || Index: {i}')
-#     dss.active_class_next()
 
 
 # Float methods
