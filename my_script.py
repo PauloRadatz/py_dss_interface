a6d2555e9ca00d189455be0888452040deb74a13
PauloRadatz <paulo.radatz@gmail.com>
2021-06-21 16:08:44 -0400

    my_script updated

--- my_script.py
+++ my_script.py
@@ -7,9 +7,9 @@ dss = py_dss_interface.DSSDLL()
 
 # Select the DSS model
 dss_file = r"C:\MeuTCC\Paulo_Example\DSSFiles\MASTER_RedeTeste13Barras.dss"
-
+dss_file = r"C:\Program Files\OpenDSS\IEEETestCases\13Bus\IEEE13Nodeckt.dss"
 # Compile
-dss.text("compile {}".format(dss_file))
+dss.text("compile [{}]".format(dss_file))
 overload_file_path = pathlib.Path(dss_file).parent.joinpath(f"{dss.circuit_name()}_EXP_OVERLOADS.CSV")
 
 # Solve
@@ -18,16 +18,16 @@ dss.text("? Load.611.kw")
 dss.solution_solve()
 
 dss.loadshapes_first()
-dss.loadshapes_read_pmult()
+dss.loadshapes_read_p_mult()
 
-new = list(dss.loadshapes_read_pmult())
+new = list(dss.loadshapes_read_p_mult())
 new[2] = 0
 
-dss.loadshapes_write_pmult(new)
+dss.loadshapes_write_p_mult(new)
 # Show Voltage Report
 
 print("worked here")
-print(dss.loadshapes_read_pmult())
+print(dss.loadshapes_read_p_mult())
 dss.text("show voltages")
 
 print(dss.dss_read_datapath())
