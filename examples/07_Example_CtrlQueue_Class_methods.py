c4419b70100a1b053f77445f7b391819fc9477df
eniocc <eniocc@gmail.com>
2021-06-21 01:42:03 -0300

    Now, the ctrlqueue_ctrlqueue method is called before clear the queue

--- examples/07_Example_CtrlQueue_Class_methods.py
+++ examples/07_Example_CtrlQueue_Class_methods.py
@@ -6,6 +6,10 @@ from py_dss_interface.models.Example.ExampleBase import ExampleBase
 
 dss = ExampleBase("13").dss
 
+# Variant methods
+print(45 * '=' + ' Variant Methods ' + 45 * '=')
+print(f'dss.ctrlqueue_ctrlqueue(): {dss.ctrlqueue_ctrlqueue()}')
+
 # Integer methods
 print(45 * '=' + ' Integer Methods' + 45 * '=')
 print(f'dss.ctrlqueue_clearqueue(): {dss.ctrlqueue_clear_queue()}')
@@ -15,13 +19,11 @@ print(f'dss.ctrlqueue_action(): {dss.ctrlqueue_action()}')
 print(f'dss.ctrlqueue_actioncode(): {dss.ctrlqueue_action_code()}')
 print(f'dss.ctrlqueue_devicehandle(): {dss.ctrlqueue_device_handle()}')
 print(f'dss.ctrlqueue_push(): {dss.ctrlqueue_push()}')
-print(f'dss.ctrlqueue_show(): {dss.ctrlqueue_show(dss)}')
+print(f'dss.ctrlqueue_show(): {dss.ctrlqueue_show()}')
 print(f'dss.ctrlqueue_clearactions(): {dss.ctrlqueue_clear_actions()}')
 print(f'dss.ctrlqueue_popaction(): {dss.ctrlqueue_pop_action()}')
 print(f'dss.ctrlqueue_queuesize(): {dss.ctrlqueue_queue_size()}')
 print(f'dss.ctrlqueue_doallqueue(): {dss.ctrlqueue_do_all_queue()}')
 
 
-# Variant methods
-print(45 * '=' + ' Variant Methods ' + 45 * '=')
-print(f'dss.ctrlqueue_ctrlqueue(): {dss.ctrlqueue_ctrlqueue()}')
+
