8cc8d2b53992abeacef66605203811f04762c75f
Ênio Viana <eniocc@gmail.com>
2022-07-23 20:50:22 -0300

    Updating all def in DSSPropreties and tests

--- examples/12_Example_DSSProperties.py
+++ examples/12_Example_DSSProperties.py
@@ -8,14 +8,14 @@ dss = ExampleBase("13").dss
 
 # General methods
 print(45 * '=' + ' General Methods' + 45 * '=')
-print(f'dss.dssproperties_name(): {dss.dssproperties_name("1")}')
-print(f'dss.dssproperties_description(): {dss.dssproperties_description("1")}')
+print(f'dss.dssproperties_name(): {dss.name("1")}')
+print(f'dss.dssproperties_description(): {dss.description("1")}')
 
-print(f'dss.dssproperties_read_value(): {dss.dssproperties_read_value("1")}')
+print(f'dss.dssproperties_read_value(): {dss.value_read("1")}')
 
-print(f'dss.dssproperties_write_value(): {dss.dssproperties_write_value("1")}')
-print(f'dss.dssproperties_read_value(): {dss.dssproperties_read_value("1")}')
+print(f'dss.dssproperties_write_value(): {dss.value_write("1")}')
+print(f'dss.dssproperties_read_value(): {dss.value_read("1")}')
 
 
-print(f'dss.dssproperties_write_value(): {dss.dssproperties_write_value("2")}')
-print(f'dss.dssproperties_read_value(): {dss.dssproperties_read_value("1")}')
+print(f'dss.dssproperties_write_value(): {dss.value_write("2")}')
+print(f'dss.dssproperties_read_value(): {dss.value_read("1")}')
