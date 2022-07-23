4b2fab30543c5aa8c95fedeb5ed278cc113e747b
Ênio Viana <eniocc@gmail.com>
2022-07-23 20:15:20 -0300

    Updating all def in CtrlQueue and tests

--- examples/07_Example_CtrlQueue.py
+++ examples/07_Example_CtrlQueue.py
@@ -8,22 +8,22 @@ dss = ExampleBase("13").dss
 
 # Variant methods
 print(45 * '=' + ' Variant Methods ' + 45 * '=')
-print(f'dss.ctrlqueue_ctrlqueue(): {dss.ctrlqueue_ctrlqueue()}')
+print(f'dss.ctrlqueue_ctrlqueue(): {dss.ctrlqueue()}')
 
 # Integer methods
 print(45 * '=' + ' Integer Methods' + 45 * '=')
-print(f'dss.ctrlqueue_clear_queue(): {dss.ctrlqueue_clear_queue()}')
-print(f'dss.ctrlqueue_delete(): {dss.ctrlqueue_delete()}')
-print(f'dss.ctrlqueue_num_actions(): {dss.ctrlqueue_num_actions()}')
-print(f'dss.ctrlqueue_action(): {dss.ctrlqueue_action()}')
-print(f'dss.ctrlqueue_action_code(): {dss.ctrlqueue_action_code()}')
-print(f'dss.ctrlqueue_device_handle(): {dss.ctrlqueue_device_handle()}')
-print(f'dss.ctrlqueue_push(): {dss.ctrlqueue_push()}')
-print(f'dss.ctrlqueue_show(): {dss.ctrlqueue_show()}')
-print(f'dss.ctrlqueue_clear_actions(): {dss.ctrlqueue_clear_actions()}')
-print(f'dss.ctrlqueue_pop_action(): {dss.ctrlqueue_pop_action()}')
-print(f'dss.ctrlqueue_queue_size(): {dss.ctrlqueue_queue_size()}')
-print(f'dss.ctrlqueue_do_all_queue(): {dss.ctrlqueue_do_all_queue()}')
+print(f'dss.ctrlqueue_clear_queue(): {dss.clear_queue()}')
+print(f'dss.ctrlqueue_delete(): {dss.delete()}')
+print(f'dss.ctrlqueue_num_actions(): {dss.num_actions()}')
+print(f'dss.ctrlqueue_action(): {dss.action()}')
+print(f'dss.ctrlqueue_action_code(): {dss.action_code()}')
+print(f'dss.ctrlqueue_device_handle(): {dss.device_handle()}')
+print(f'dss.ctrlqueue_push(): {dss.push()}')
+print(f'dss.ctrlqueue_show(): {dss.show()}')
+print(f'dss.ctrlqueue_clear_actions(): {dss.clear_actions()}')
+print(f'dss.ctrlqueue_pop_action(): {dss.pop_action()}')
+print(f'dss.ctrlqueue_queue_size(): {dss.queue_size()}')
+print(f'dss.ctrlqueue_do_all_queue(): {dss.do_all_queue()}')
 
 
 
