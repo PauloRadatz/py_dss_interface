5eabdabbdc309827c8742d3e0ec89f62c842e304
Ênio Viana <eniocc@gmail.com>
2022-07-23 20:49:38 -0300

    Updating all def in DSSProgress and tests

--- src/py_dss_interface/models/DSSProgress/DSSProgressI.py
+++ src/py_dss_interface/models/DSSProgress/DSSProgressI.py
@@ -18,14 +18,14 @@ class DSSProgressI(Base):
     which can be one of the following.
     """
 
-    def dssprogress_pct_progress(self, arg: float) -> int:
+    def pct_progress(self, arg: float) -> int:
         """Sets the percent progress to indicate [0..100]."""
         return int(self.dss_obj.DSSProgressI(ctypes.c_int32(0), ctypes.c_float(arg)))
 
-    def dssprogress_show(self) -> int:
+    def show(self) -> int:
         """Shows progress form with null caption and progress set to zero."""
         return int(self.dss_obj.DSSProgressI(ctypes.c_int32(1), ctypes.c_int32(0)))
 
-    def dssprogress_close(self) -> int:
+    def close(self) -> int:
         """Closes (hides) DSS Progress form."""
         return int(self.dss_obj.DSSProgressI(ctypes.c_int32(2), ctypes.c_int32(0)))
