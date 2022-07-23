65bb6ed99f82f636fa37c5e8818b8c4e4e170212
Ênio Viana <eniocc@gmail.com>
2022-07-23 17:20:04 -0300

    Updating all def in CapControls and tests

--- src/py_dss_interface/models/CapControls/CapControlsI.py
+++ src/py_dss_interface/models/CapControls/CapControlsI.py
@@ -16,15 +16,15 @@ class CapControlsI(Base):
     which can be one of the following
     """
 
-    def capcontrols_first(self) -> int:
+    def first(self) -> int:
         """Sets the first CapControl active. Returns 0 if no more."""
         return self.dss_obj.CapControlsI(0, 0)
 
-    def capcontrols_next(self) -> int:
+    def next(self) -> int:
         """Sets the next CapControl active. Returns 0 if no more."""
         return self.dss_obj.CapControlsI(1, 0)
 
-    def capcontrols_read_mode(self) -> int:
+    def read_mode(self) -> int:
         """Gets the type of automatic controller (see manual for details).
         CURRENTCONTROL: Result := 0;
         VOLTAGECONTROL: Result := 1;
@@ -35,7 +35,7 @@ class CapControlsI(Base):
         """
         return self.dss_obj.CapControlsI(2, 0)
 
-    def capcontrols_write_mode(self, argument: int) -> int:
+    def write_mode(self, argument: int) -> int:
         """Sets the type of automatic controller (see manual for details).
         0: elem.CapControlType := CURRENTCONTROL;
         1: elem.CapControlType := VOLTAGECONTROL;
@@ -45,38 +45,38 @@ class CapControlsI(Base):
         """
         return self.dss_obj.CapControlsI(3, argument)
 
-    def capcontrols_read_monitored_term(self) -> int:
+    def read_monitored_term(self) -> int:
         """Gets the terminal number on the element that PT and CT are connected to."""
         return self.dss_obj.CapControlsI(4, 0)
 
-    def capcontrols_write_monitored_term(self, dss, argument: int) -> int:
+    def write_monitored_term(self, dss, argument: int) -> int:
         """Sets the terminal number on the element that PT and CT are connected to. There is not a explicit return
         type in the oficial documentation, because of this we choose not put a explicit return too. """
         return (
             dss.text(
-                f'edit CapControl.{dss.capcontrols_read_name()} Terminal={argument}'
+                f'edit CapControl.{dss.read_name()} Terminal={argument}'
             )
-            if self.capcontrols_count() != 0
+            if self.count() != 0
             else 0
         )
 
     # TODO
-    def capcontrols_read_use_volt_override(self) -> int:
+    def read_use_volt_override(self) -> int:
         """Gets if Vmin and Vmax are enabled to override the control Mode. There is not a explicit return type in the
         oficial documentation, because of this we choose not put a explicit return too."
         return self.dss_obj.CapControlsI(6, 0). """
 
-    def capcontrols_write_use_volt_override(self, dss, argument: int) -> int:
+    def write_use_volt_override(self, dss, argument: int) -> int:
         """Sets if enables Vmin and Vmax to override the control Mode. There is not a explicit return type in the
         oficial documentation, because of this we choose not put a explicit return too. """
         return (
             dss.text(
-                f'edit CapControl.{dss.capcontrols_read_name()} VoltOverride={argument}'
+                f'edit CapControl.{dss.read_name()} VoltOverride={argument}'
             )
-            if self.capcontrols_count() != 0
+            if self.count() != 0
             else 0
         )
 
-    def capcontrols_count(self) -> int:
+    def count(self) -> int:
         """Gets the number of CapControls in Active Circuit."""
         return self.dss_obj.CapControlsI(8, 0)
--- tests/py_dss_interface/test_capcontrols.py
+++ tests/py_dss_interface/test_capcontrols.py
@@ -31,7 +31,7 @@ class TestCapControls13Bus:
             "VoltOverride=Y Vmin=7110 Vmax=7740 Delay=102 Delayoff=102")
         dss.solution_solve()
 
-        dss.capcontrols_write_name("CAPBank2C_Ctrl")
+        dss.write_name("CAPBank2C_Ctrl")
 
         return dss
 
@@ -40,49 +40,49 @@ class TestCapControls13Bus:
     # ===================================================================
     def test_capcontrols_first(self, dss):
         expected = 1
-        actual = dss.capcontrols_first()
+        actual = dss.first()
         assert actual == expected
 
     def test_capcontrols_next(self, dss):
         expected = 2
-        dss.capcontrols_first()
-        actual = dss.capcontrols_next()
+        dss.first()
+        actual = dss.next()
         assert actual == expected
 
     def test_capcontrols_count(self, dss):
         expected = 3
-        actual = dss.capcontrols_count()
+        actual = dss.count()
         assert actual == expected
 
     def test_capcontrols_read_mode(self, dss):
         expected = 2
-        actual = dss.capcontrols_read_mode()
+        actual = dss.read_mode()
         assert actual == expected
 
     def test_capcontrols_write_mode(self, dss):
         expected = 3
-        dss.capcontrols_write_mode(expected)
-        actual = dss.capcontrols_read_mode()
+        dss.write_mode(expected)
+        actual = dss.read_mode()
         assert actual == expected
 
     def test_capcontrols_read_monitored_term(self, dss):
         expected = 1
-        actual = dss.capcontrols_read_monitored_term()
+        actual = dss.read_monitored_term()
         assert actual == expected
 
     def test_capcontrols_write_monitored_term(self, dss):
         expected = 1
-        dss.capcontrols_write_monitored_term(dss, expected)
-        actual = dss.capcontrols_read_monitored_term()
+        dss.write_monitored_term(dss, expected)
+        actual = dss.read_monitored_term()
         assert actual == expected
 
     def test_capcontrols_read_use_volt_override(self, dss):
-        actual = dss.capcontrols_read_use_volt_override()
+        actual = dss.read_use_volt_override()
         assert actual is None
 
     def test_capcontrols_write_use_volt_override(self, dss):
-        dss.capcontrols_write_use_volt_override(dss, 10)
-        actual = dss.capcontrols_read_use_volt_override()
+        dss.write_use_volt_override(dss, 10)
+        actual = dss.read_use_volt_override()
         assert actual is None
 
     # ===================================================================
@@ -90,35 +90,35 @@ class TestCapControls13Bus:
     # ===================================================================
     def test_capcontrols_read_name(self, dss):
         expected = 'CAPBank2C_Ctrl'.lower()
-        actual = dss.capcontrols_read_name()
+        actual = dss.read_name()
         assert actual == expected
 
     def test_capcontrols_write_name(self, dss):
         expected = 'CAPBank2B_Ctrl'.lower()
-        dss.capcontrols_write_name(expected)
-        actual = dss.capcontrols_read_name()
+        dss.write_name(expected)
+        actual = dss.read_name()
         assert actual == expected
 
     def test_capcontrols_read_capacitor(self, dss):
         expected = 'CAPBank2C'.lower()
-        actual = dss.capcontrols_read_capacitor()
+        actual = dss.capacitor_controlled()
         assert actual == expected
 
     def test_capcontrols_write_capacitor(self, dss):
         expected = 'CAPBank2B'.lower()
-        dss.capcontrols_write_capacitor(expected)
-        actual = dss.capcontrols_read_capacitor()
+        dss.write_capacitor(expected)
+        actual = dss.capacitor_controlled()
         assert actual == expected
 
     def test_capcontrols_read_monitored_obj(self, dss):
         expected = 'line.670671'
-        actual = dss.capcontrols_read_monitored_obj()
+        actual = dss.monitored_object()
         assert actual == expected
 
     def test_capcontrols_write_monitored_obj(self, dss):
         expected = 'line.671680'
-        dss.capcontrols_write_monitored_obj(expected)
-        actual = dss.capcontrols_read_monitored_obj()
+        dss.monitored_obj_write(expected)
+        actual = dss.monitored_object()
         assert actual == expected
 
     # ===================================================================
@@ -126,101 +126,101 @@ class TestCapControls13Bus:
     # ===================================================================
     def test_capcontrols_read_ct_ratio(self, dss):
         expected = 1.0
-        actual = dss.capcontrols_read_ct_ratio()
+        actual = dss.ct_ratio_read()
         assert actual == expected
 
     def test_capcontrols_write_ct_ratio(self, dss):
         expected = 1.0
-        dss.capcontrols_write_ct_ratio(expected)
-        actual = dss.capcontrols_read_ct_ratio()
+        dss.ct_ratio_write(expected)
+        actual = dss.ct_ratio_read()
         assert actual == expected
 
     def test_capcontrols_read_pt_ratio(self, dss):
         expected = 1.0
-        actual = dss.capcontrols_read_pt_ratio()
+        actual = dss.pt_ratio_read()
         assert actual == expected
 
     def test_capcontrols_write_pt_ratio(self, dss):
         expected = 1.0
-        dss.capcontrols_write_pt_ratio(expected)
-        actual = dss.capcontrols_read_pt_ratio()
+        dss.pt_ratio_write(expected)
+        actual = dss.pt_ratio_read()
         assert actual == expected
 
     def test_capcontrols_read_on_setting(self, dss):
         expected = 150.0
-        actual = dss.capcontrols_read_on_setting()
+        actual = dss.on_setting_read()
         assert actual == expected
 
     def test_capcontrols_write_on_setting(self, dss):
         expected = 150.0
-        dss.capcontrols_write_on_setting(expected)
-        actual = dss.capcontrols_read_on_setting()
+        dss.on_setting_write(expected)
+        actual = dss.on_setting_read()
         assert actual == expected
 
     def test_capcontrols_read_off_setting(self, dss):
         expected = -225
-        actual = dss.capcontrols_read_off_setting()
+        actual = dss.off_setting_read()
         assert actual == expected
 
     def test_capcontrols_write_off_setting(self, dss):
         expected = 150.0
-        dss.capcontrols_write_off_setting(expected)
-        actual = dss.capcontrols_read_off_setting()
+        dss.off_setting_write(expected)
+        actual = dss.off_setting_read()
         assert actual == expected
 
     def test_capcontrols_read_vmax(self, dss):
         expected = 7740
-        actual = dss.capcontrols_read_vmax()
+        actual = dss.vmax_read()
         assert actual == expected
 
     def test_capcontrols_write_vmax(self, dss):
         expected = 8000
-        dss.capcontrols_write_vmax(expected)
-        actual = dss.capcontrols_read_vmax()
+        dss.vmax_write(expected)
+        actual = dss.vmax_read()
         assert actual == expected
 
     def test_capcontrols_read_vmin(self, dss):
         expected = 7110
-        actual = dss.capcontrols_read_vmin()
+        actual = dss.vmin_read()
         assert actual == expected
 
     def test_capcontrols_write_vmin(self, dss):
         expected = 7000
-        dss.capcontrols_write_vmin(expected)
-        actual = dss.capcontrols_read_vmin()
+        dss.vmin_write(expected)
+        actual = dss.vmin_read()
         assert actual == expected
 
     def test_capcontrols_read_delay(self, dss):
         expected = 102
-        actual = dss.capcontrols_read_delay()
+        actual = dss.delay_read()
         assert actual == expected
 
     def test_capcontrols_write_delay(self, dss):
         expected = 105
-        dss.capcontrols_write_delay(expected)
-        actual = dss.capcontrols_read_delay()
+        dss.delay_write(expected)
+        actual = dss.delay_read()
         assert actual == expected
 
     def test_capcontrols_read_delay_off(self, dss):
         expected = 102
-        actual = dss.capcontrols_read_delay_off()
+        actual = dss.delay_off_read()
         assert actual == expected
 
     def test_capcontrols_write_delay_off(self, dss):
         expected = 105
-        dss.capcontrols_write_delay_off(expected)
-        actual = dss.capcontrols_read_delay_off()
+        dss.delay_off_write(expected)
+        actual = dss.delay_off_read()
         assert actual == expected
 
     def test_capcontrols_read_dead_time(self, dss):
         expected = 300
-        actual = dss.capcontrols_read_dead_time()
+        actual = dss.dead_time_read()
         assert actual == expected
 
     def test_capcontrols_write_dead_time(self, dss):
         expected = 10
-        dss.capcontrols_write_dead_time(expected)
-        actual = dss.capcontrols_read_dead_time()
+        dss.dead_time_write(expected)
+        actual = dss.dead_time_read()
         assert actual == expected
 
     # ===================================================================
@@ -228,5 +228,5 @@ class TestCapControls13Bus:
     # ===================================================================
     def test_capcontrols_all_names(self, dss):
         expected = [x.lower() for x in ['CAPBank2A_Ctrl', 'CAPBank2B_Ctrl', 'CAPBank2C_Ctrl']]
-        actual = dss.capcontrols_all_names()
+        actual = dss.all_names()
         assert actual == expected
