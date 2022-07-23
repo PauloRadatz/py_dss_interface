1c2f033818836a2588642dab6619036846f94a9a
Ênio Viana <eniocc@gmail.com>
2022-07-23 20:16:03 -0300

    Updating all def in DSSElement and tests

--- examples/08_Example_DSSElement.py
+++ examples/08_Example_DSSElement.py
@@ -8,13 +8,13 @@ dss = ExampleBase("13").dss
 
 # Integer methods
 print(45 * '=' + ' Integer Methods' + 45 * '=')
-print(f'dsselement_num_properties: {dss.dsselement_num_properties()}')
+print(f'dsselement_num_properties: {dss.num_properties()}')
 
 # String methods
 print(45 * '=' + ' String Methods ' + 45 * '=')
-print(f'dsselement_name: {dss.dsselement_name()}')
+print(f'dsselement_name: {dss.name()}')
 
 
 # Variant methods
 print(45 * '=' + ' Variant Methods ' + 45 * '=')
-print(f'dsselement_all_property_names: {dss.dsselement_all_property_names()}')
+print(f'dsselement_all_property_names: {dss.property_names()}')
