0f5303c65313a0e5975cd683d96f7bc521a214f4
Ênio Viana <eniocc@gmail.com>
2022-07-23 23:13:53 -0300

    Updating all def in Bus and tests. Now we have properties instead functions.

--- src/py_dss_interface/models/Bus/Bus.py
+++ src/py_dss_interface/models/Bus/Bus.py
@@ -2,6 +2,7 @@
 """
  Created by eniocc at 11/10/2020
 """
+from typing import List
 
 from py_dss_interface.models.Bus.BusF import BusF
 from py_dss_interface.models.Bus.BusI import BusI
@@ -14,4 +15,182 @@ class Bus(BusS, BusI, BusV, BusF):
     This interface implements the Bus (IBus) interface of OpenDSS by declaring 4 procedures for accessing the
     different properties included in this interface: BusS, BusI, BusV, BusF.
     """
-    pass
+
+    def __init__(self, obj_dss):
+        super().__init__(obj_dss)
+
+    @property
+    def name(self) -> str:
+        return BusS._name(self)
+
+    @property
+    def num_nodes(self) -> int:
+        return BusI._num_nodes(self)
+
+    @property
+    def zsc_refresh(self) -> int:
+        return BusI._zsc_refresh(self)
+
+    @property
+    def coord_defined(self) -> int:
+        return BusI._coord_defined(self)
+
+    @property
+    def unique_node_number(self) -> int:
+        return BusI._unique_node_number(self, 1)
+
+    @unique_node_number.setter
+    def unique_node_number(self, start_number: int) -> int:
+        return BusI._unique_node_number(self, start_number)
+
+    @property
+    def total_customers(self) -> int:
+        return BusI._total_customers(self)
+
+    @property
+    def section_id(self) -> int:
+        return BusI._section_id(self)
+
+    @property
+    def voltages(self):
+        return BusV._voltages(self)
+
+    @property
+    def seq_voltages(self):
+        return BusV._seq_voltages(self)
+
+    @property
+    def nodes(self) -> List[int]:
+        return BusV._nodes(self)
+
+    @property
+    def voc(self):
+        return BusV._voc(self)
+
+    @property
+    def isc(self):
+        return BusV._isc(self)
+
+    @property
+    def pu_voltages(self):
+        return BusV._pu_voltages(self)
+
+    @property
+    def zsc_matrix(self):
+        return BusV._zsc_matrix(self)
+
+    @property
+    def zsc1(self):
+        return BusV._zsc1(self)
+
+    @property
+    def zsc0(self):
+        return BusV._zsc0(self)
+
+    @property
+    def ysc_matrix(self):
+        return BusV._ysc_matrix(self)
+
+    @property
+    def cplx_sequence_voltages(self):
+        return BusV._cplx_sequence_voltages(self)
+
+    @property
+    def vll(self):
+        return BusV._vll(self)
+
+    @property
+    def pu_vll(self):
+        return BusV._pu_vll(self)
+
+    @property
+    def vmag_angle(self):
+        return BusV._vmag_angle(self)
+
+    @property
+    def vmag_angle_pu(self):
+        return BusV._vmag_angle_pu(self)
+
+    @property
+    def line_list(self):
+        return BusV._line_list(self)
+
+    @property
+    def load_list(self):
+        return BusV._load_list(self)
+
+    @property
+    def axc_012_matrix(self):
+        return BusV._axc_012_matrix(self)
+
+    @property
+    def all_pce_active_bus(self) -> List[str]:
+        return BusV._all_pce_active_bus(self)
+
+    @property
+    def all_pde_active_bus(self) -> List[str]:
+        return BusV._all_pde_active_bus(self)
+
+    @property
+    def kv_base(self) -> float:
+        return BusF._kv_base(self)
+
+    @property
+    def x(self) -> float:
+        return BusF._x(self)
+
+    @x.setter
+    def x(self, value: float):
+        BusF._x_write(self, value)
+
+    @property
+    def y(self) -> float:
+        return BusF._y(self)
+
+    @y.setter
+    def y(self, value: float):
+        BusF._y_write(self, value)
+
+    @property
+    def distance(self) -> float:
+        return BusF._distance(self)
+
+    @property
+    def bus_lambda(self) -> float:
+        return BusF._bus_lambda(self)
+
+    @property
+    def interruptions_num(self) -> float:
+        return BusF._interruptions_num(self)
+
+    @property
+    def interruptions_avg_duration(self) -> float:
+        return BusF._interruptions_avg_duration(self)
+
+    @property
+    def interruptions_total_customers(self) -> float:
+        return BusF._interruptions_total_customers(self)
+
+    @property
+    def outage_customer_accum_duration(self) -> float:
+        return BusF._outage_customer_accum_duration(self)
+
+    @property
+    def line_total_miles(self) -> float:
+        return BusF._line_total_miles(self)
+
+    @property
+    def latitude(self) -> float:
+        return BusF._latitude(self)
+
+    @latitude.setter
+    def latitude(self, value: float):
+        BusF._latitude_write(self, value)
+
+    @property
+    def longitude(self) -> float:
+        return BusF._longitude(self)
+
+    @longitude.setter
+    def longitude(self, value: float):
+        BusF._longitude_write(self, value)
--- src/py_dss_interface/models/Bus/BusV.py
+++ src/py_dss_interface/models/Bus/BusV.py
@@ -19,86 +19,86 @@ class BusV(Base):
     one of the following.
     """
 
-    def voltages(self):
+    def _voltages(self):
         """Returns a complex array of voltages at this bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 0, None, '')
 
-    def seq_voltages(self):
+    def _seq_voltages(self):
         """Returns a complex array of Sequence voltages at this bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 1, None, '')
 
-    def nodes(self) -> List[int]:
+    def _nodes(self) -> List[int]:
         """Returns an integer array of node numbers defined at the bus in same order as the voltages."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 2, None, '')
 
-    def voc(self):
+    def _voc(self):
         """Returns the open circuit voltage as complex array."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 3, None, '')
 
-    def isc(self):
+    def _isc(self):
         """Returns the short circuit current as complex array."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 4, None, '')
 
-    def pu_voltages(self):
+    def _pu_voltages(self):
         """Returns the voltages in per unit at bus as complex array."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 5, None, '')
 
-    def zsc_matrix(self):
+    def _zsc_matrix(self):
         """Returns the complex array of Zsc matrix at bus, column by column."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 6, None, '')
 
-    def zsc1(self):
+    def _zsc1(self):
         """Returns the complex array of Zsc matrix at bus, column by column."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 7, None, '')
 
-    def zsc0(self):
+    def _zsc0(self):
         """Returns the complex zero-sequence short circuit impedance at bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 8, None, '')
 
-    def ysc_matrix(self):
+    def _ysc_matrix(self):
         """Returns the complex array of Ysc matrix at bus, column by column."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 9, None, '')
 
-    def cplx_sequence_voltages(self):
+    def _cplx_sequence_voltages(self):
         """Returns the complex double array of sequence voltages (0, 1, 2) at this bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 10, None, '')
 
-    def vll(self):
+    def _vll(self):
         """For 2 and 3 phase buses, returns a variant array of complex numbers representing L-L voltages in volts.
         Returns -1.0 for 1-phase bus. If more than 3 phases, returns only first 3."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 11, None, '')
 
-    def pu_vll(self):
+    def _pu_vll(self):
         """Returns a variant array of complex numbers representing L-L voltages in per unit. Returns -1.0 for 1-phase
         bus. If more than 3 phases, returns only first 3.. """
         return Bridge.var_array_function(self.dss_obj.BUSV, 12, None, '')
 
-    def vmag_angle(self):
+    def _vmag_angle(self):
         """Returns a variant array of doubles containing voltages in magnitude (VLN), angle (deg)."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 13, None, '')
 
-    def vmag_angle_pu(self):
+    def _vmag_angle_pu(self):
         """Returns a variant array of doubles containing voltages in per unit and angles in degrees."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 14, None, '')
 
-    def line_list(self):
+    def _line_list(self):
         """This parameter returns a variant array of strings containing the names of the lines connected to the
         active bus. The names of the lines include the class name 'Line.' """
         return Bridge.var_array_function(self.dss_obj.BUSV, 15, None, '')
 
-    def load_list(self):
+    def _load_list(self):
         """This parameter returns a variant array of strings containing the names of the loads connected to the
         active bus. The names of the lines include the class name 'Load.'. """
         return Bridge.var_array_function(self.dss_obj.BUSV, 16, None, '')
 
-    def axc_012_matrix(self):
+    def _axc_012_matrix(self):
         """Variant array of doubles (complex) containing the complete 012 Zsc matrix."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 17, None, '')
 
-    def all_pce_active_bus(self) -> List[str]:
+    def _all_pce_active_bus(self) -> List[str]:
         """Returns an array with the names of all PCE connected to the active bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 18, None, '')
 
-    def all_pde_active_bus(self) -> List[str]:
+    def _all_pde_active_bus(self) -> List[str]:
         """Returns an array with the names of all PDE connected to the active bus."""
         return Bridge.var_array_function(self.dss_obj.BUSV, 19, None, '')
