c1089f8792b461533fdfe6d118e0294807ea18e1
Ênio Viana <eniocc@gmail.com>
2022-07-23 21:31:42 -0300

    Updating all def in Loads and tests

--- src/py_dss_interface/models/Loads/LoadsV.py
+++ src/py_dss_interface/models/Loads/LoadsV.py
@@ -21,16 +21,16 @@ class LoadsV(Base):
     to be used and the variable “argument” (Variant) is used to return the variant structure.
     """
 
-    def loads_all_names(self):
+    def names(self):
         """Allows to read the names of all the loads present in the active circuit. The result is delivered as
         variant, however, the content of this variant is an array of strings. """
         return Bridge.var_array_function(self.dss_obj.DSSLoadsV, 0, None, '')
 
-    def loads_read_zipv(self):
+    def zipv_read(self):
         """Allows to read the array of 7 elements (doubles) for ZIP property of the active Load object."""
         return Bridge.var_array_function(self.dss_obj.DSSLoadsV, 1, None, '')
 
-    def loads_write_zipv(self, argument):
+    def zipv_write(self, argument):
         """Allows to write the array of 7 elements (doubles) for ZIP property of the active Load object.
         :param argument: Array of 7 coefficients:
                     First 3 are ZIP weighting factors for real power (should sum to 1)
@@ -41,5 +41,5 @@ class LoadsV(Base):
         argument = Base.check_string_param(argument)
         t = Text(self.dss_obj)
         load = LoadsS(self.dss_obj)
-        load_name = load.loads_read_name()
+        load_name = load.name_read()
         return t.text(f'edit Load.{load_name} zipv = {argument}')
