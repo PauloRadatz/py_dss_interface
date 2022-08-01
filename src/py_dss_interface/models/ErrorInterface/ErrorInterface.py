97978becf2c4d825a753b4e456b5fa9c43fcd4ac
Ênio Viana <eniocc@gmail.com>
2022-08-01 17:43:32 -0300

    Refactoring ErrorInterface to new arch

--- src/py_dss_interface/models/ErrorInterface/ErrorInterface.py
+++ src/py_dss_interface/models/ErrorInterface/ErrorInterface.py
@@ -8,7 +8,17 @@ from py_dss_interface.models.ErrorInterface.ErrorDesc import ErrorDesc
 
 class ErrorOpenDSS(ErrorCode, ErrorDesc):
     """
-    This interface implements the ErrorOpenDSS interface of OpenDSS by declaring 2 procedures for accessing the different
-    properties included in this interface.
+    This interface implements the ErrorOpenDSS interface of OpenDSS by declaring 2 procedures for accessing the
+    different properties included in this interface.
     """
-    pass
+
+    def __init__(self, obj_dss):
+        super().__init__(obj_dss)
+
+    @property
+    def error_code(self):
+        return ErrorCode.error_code(self)
+
+    @property
+    def error_desc(self):
+        return ErrorDesc.error_desc(self)
