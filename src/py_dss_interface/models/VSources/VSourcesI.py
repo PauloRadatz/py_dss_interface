a236747d11546029d20b669c3b0ac68c67cc367c
Rodolfo Londero <rodolfo.londero@smarthis.com.br>
2022-08-08 20:51:20 -0300

    Refactoring VSources to new arch

--- src/py_dss_interface/models/VSources/VSourcesI.py
+++ src/py_dss_interface/models/VSources/VSourcesI.py
@@ -18,23 +18,23 @@ class VSourcesI(Base):
     Parameter, which can be one of the following.
     """
 
-    def vsources_count(self) -> int:
+    def _count(self) -> int:
         """Returns the number of VSource objects currently defined in the active circuit."""
         return self.dss_obj.VsourcesI(ctypes.c_int32(0), ctypes.c_int32(0))
 
-    def vsources_first(self) -> int:
+    def _first(self) -> int:
         """Sets the first VSource to be active; returns 0 if none."""
         return self.dss_obj.VsourcesI(ctypes.c_int32(1), ctypes.c_int32(0))
 
-    def vsources_next(self) -> int:
+    def _next(self) -> int:
         """Sets the next VSource to be active; returns 0 if none."""
         return self.dss_obj.VsourcesI(ctypes.c_int32(2), ctypes.c_int32(0))
 
-    def vsources_read_phases(self) -> int:
+    def _phases(self) -> int:
         """Gets the number of phases of the active VSource."""
         return self.dss_obj.VsourcesI(ctypes.c_int32(3), ctypes.c_int32(0))
 
-    def vsources_write_phases(self, argument) -> int:
+    def _phases_write(self, argument: int) -> int:
         """Sets the number of phases of the active VSource."""
         argument = Base.check_int_param(argument)
         return self.dss_obj.VsourcesI(ctypes.c_int32(4), ctypes.c_int32(argument))
