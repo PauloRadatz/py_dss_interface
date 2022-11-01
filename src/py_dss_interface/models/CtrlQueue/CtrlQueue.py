c0eb3295f6a1d5c388e4ba4f23ec6e8fcf2079f9
Ênio Viana <eniocc@gmail.com>
2022-11-01 15:38:35 -0300

    Updating CtrlQueue

--- src/py_dss_interface/models/CtrlQueue/CtrlQueue.py
+++ src/py_dss_interface/models/CtrlQueue/CtrlQueue.py
@@ -11,4 +11,55 @@ class CtrlQueue(CtrlQueueI, CtrlQueueV):
     This interface implements the CtrlQueue (ICtrlQueue) interface of OpenDSS by declaring 2 procedures for accessing
     the different properties included in this interface: CtrlQueueI, CtrlQueueV
     """
-    pass
+
+    def __init__(self, obj_dss):
+        super().__init__(obj_dss)
+
+    def clear_queue(self) -> int:
+        return CtrlQueueI._clear_queue(self)
+
+    @staticmethod
+    def delete(argument: int) -> int:
+        return CtrlQueueI._delete(argument)
+
+    @property
+    def num_actions(self) -> int:
+        return CtrlQueueI._num_actions(self)
+
+    def action(self, argument: int) -> int:
+        return CtrlQueueI._action(self, argument)
+
+    @property
+    def action_code(self) -> int:
+        return CtrlQueueI._action_code(self)
+
+    @property
+    def device_handle(self) -> int:
+        return CtrlQueueI._device_handle(self)
+
+    @property
+    def push(self) -> int:
+        return CtrlQueueI._push(self)
+
+    @property
+    def show(self) -> int:
+        return CtrlQueueI._show(self)
+
+    @property
+    def clear_actions(self) -> int:
+        return CtrlQueueI._clear_actions(self)
+
+    @property
+    def pop_action(self) -> int:
+        return CtrlQueueI._pop_action(self)
+
+    @property
+    def queue_size(self) -> int:
+        return CtrlQueueI._queue_size(self)
+
+    @property
+    def do_all_queue(self) -> int:
+        return CtrlQueueI._do_all_queue(self)
+
+    def ctrlqueue(self) -> str:
+        return CtrlQueueV._ctrlqueue(self)
