b8114c712e5665e7a79de17d0a0a0dfaa183f2f5
PauloRadatz <pradatz@epri.com>
2022-11-17 16:41:29 -0500

    Transformers

--- src/py_dss_interface/models/Transformers/Transformers.py
+++ src/py_dss_interface/models/Transformers/Transformers.py
@@ -7,11 +7,177 @@ from py_dss_interface.models.Transformers.TransformersF import TransformersF
 from py_dss_interface.models.Transformers.TransformersI import TransformersI
 from py_dss_interface.models.Transformers.TransformersS import TransformersS
 from py_dss_interface.models.Transformers.TransformersV import TransformersV
-
+from typing import List
 
 class Transformers(TransformersV, TransformersF, TransformersI, TransformersS):
     """
     This interface implements the Transformers (ITransformer) interface of OpenDSS by declaring 4 procedures for
     accessing the different properties included in this interface: TransformersV, TransformersF, TransformersI, TransformersS.
     """
-    pass
+
+    def __init__(self, obj_dss):
+        super().__init__(obj_dss)
+
+    @property
+    def r(self) -> float:
+        return TransformersF._r_read(self)
+
+    @r.setter
+    def r(self, argument: float):
+        TransformersF._r_write(self, argument)
+
+    @property
+    def tap(self) -> float:
+        return TransformersF._tap_read(self)
+
+    @tap.setter
+    def tap(self, argument: float):
+        TransformersF._tap_write(self, argument)
+
+    @property
+    def min_tap(self) -> float:
+        return TransformersF._min_tap_read(self)
+
+    @min_tap.setter
+    def min_tap(self, argument: float):
+        TransformersF._min_tap_write(self, argument)
+
+    @property
+    def max_tap(self) -> float:
+        return TransformersF._max_tap_read(self)
+
+    @max_tap.setter
+    def max_tap(self, argument: float):
+        TransformersF._max_tap_write(self, argument)
+
+    @property
+    def kv(self) -> float:
+        return TransformersF._kv_read(self)
+
+    @kv.setter
+    def kv(self, argument: float):
+        TransformersF._kv_write(self, argument)
+
+    @property
+    def kva(self) -> float:
+        return TransformersF._kva_read(self)
+
+    @kva.setter
+    def kva(self, argument: float):
+        TransformersF._kva_write(self, argument)
+
+    @property
+    def x_neut(self) -> float:
+        return TransformersF._x_neut_read(self)
+
+    @x_neut.setter
+    def x_neut(self, argument: float):
+        TransformersF._x_neut_write(self, argument)
+
+    @property
+    def r_neut(self) -> float:
+        return TransformersF._r_neut_read(self)
+
+    @r_neut.setter
+    def r_neut(self, argument: float):
+        TransformersF._r_neut_write(self, argument)
+
+    @property
+    def xhl(self) -> float:
+        return TransformersF._xhl_read(self)
+
+    @xhl.setter
+    def xhl(self, argument: float):
+        TransformersF._xhl_write(self, argument)
+
+    @property
+    def xht(self) -> float:
+        return TransformersF._xht_read(self)
+
+    @xht.setter
+    def xht(self, argument: float):
+        TransformersF._xht_write(self, argument)
+
+    @property
+    def xlt(self) -> float:
+        return TransformersF._xlt_read(self)
+
+    @xlt.setter
+    def xlt(self, argument: float):
+        TransformersF._xlt_write(self, argument)
+
+    @property
+    def num_windings(self) -> int:
+        return TransformersI._num_windings_read(self)
+
+    @num_windings.setter
+    def num_windings(self, argument: int):
+        TransformersI._num_windings_write(self, argument)
+
+    @property
+    def wdg(self) -> int:
+        return TransformersI._wdg_read(self)
+
+    @wdg.setter
+    def wdg(self, argument: int):
+        TransformersI._wdg_write(self, argument)
+
+    @property
+    def num_taps(self) -> int:
+        return TransformersI._num_taps_read(self)
+
+    @num_taps.setter
+    def num_taps(self, argument: int):
+        TransformersI._num_taps_write(self, argument)
+
+    @property
+    def is_delta(self) -> int:
+        return TransformersI._is_delta_read(self)
+
+    @is_delta.setter
+    def is_delta(self, argument: int):
+        TransformersI._is_delta_write(self, argument)
+
+    def first(self) -> int:
+        return TransformersI._first(self)
+
+    def next(self) -> int:
+        return TransformersI._next(self)
+
+    @property
+    def count(self) -> int:
+        return TransformersI._count(self)
+
+    @property
+    def xfmr_code(self) -> str:
+        return TransformersS._xfmr_code_read(self)
+
+    @xfmr_code.setter
+    def xfmr_code(self, argument: str):
+        TransformersS._xfmr_code_write(self, argument)
+
+    @property
+    def name(self) -> str:
+        return TransformersS._name_read(self)
+
+    @name.setter
+    def name(self, argument: str):
+        TransformersS._name_write(self, argument)
+
+    @property
+    def str_wdg_voltages(self) -> str:
+        return TransformersS._str_wdg_voltages(self)
+
+    @property
+    def names(self) -> List[str]:
+        return TransformersV._names(self)
+
+    @property
+    def wdg_voltages(self) -> List[float]:
+        return TransformersV._wdg_voltages(self)
+
+    @property
+    def wdg_currents(self) -> List[float]:
+        return TransformersV._wdg_currents(self)
+
+
--- src/py_dss_interface/models/Transformers/TransformersF.py
+++ src/py_dss_interface/models/Transformers/TransformersF.py
@@ -21,7 +21,7 @@ class TransformersF(Base):
     The properties (parameter) are integer numbers and are described as follows.
     """
 
-    def _r(self) -> float:
+    def _r_read(self) -> float:
         """Gets the active winding resistance in %."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(0), ctypes.c_double(0)))
 
@@ -30,7 +30,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(1), ctypes.c_double(argument)))
 
-    def _tap(self) -> float:
+    def _tap_read(self) -> float:
         """Gets the active winding tap in per-unit."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(2), ctypes.c_double(0)))
 
@@ -39,7 +39,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(3), ctypes.c_double(argument)))
 
-    def _min_tap(self) -> float:
+    def _min_tap_read(self) -> float:
         """Gets the active winding minimum tap in per-unit."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(4), ctypes.c_double(0)))
 
@@ -48,7 +48,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(5), ctypes.c_double(argument)))
 
-    def _max_tap(self) -> float:
+    def _max_tap_read(self) -> float:
         """Gets the active winding maximum tap in per-unit."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(6), ctypes.c_double(0)))
 
@@ -57,7 +57,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(7), ctypes.c_double(argument)))
 
-    def _kv(self) -> float:
+    def _kv_read(self) -> float:
         """Gets the active winding kV rating. Phase-phase for 2 or 3 phases, actual winding kV 1 phase transformer."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(8), ctypes.c_double(0)))
 
@@ -66,7 +66,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(9), ctypes.c_double(argument)))
 
-    def _kva(self) -> float:
+    def _kva_read(self) -> float:
         """Gets the active winding kVA rating. On winding 1, this also determines normal and emergency current
         ratings for all windings. """
         return float(self.dss_obj.TransformersF(ctypes.c_int32(10), ctypes.c_double(0)))
@@ -77,7 +77,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(11), ctypes.c_double(argument)))
 
-    def _x_neut(self) -> float:
+    def _x_neut_read(self) -> float:
         """Gets the active winding neutral reactance [ohms] for wye connections."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(12), ctypes.c_double(0)))
 
@@ -86,7 +86,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(13), ctypes.c_double(argument)))
 
-    def _r_neut(self) -> float:
+    def _r_neut_read(self) -> float:
         """Gets the active winding neutral resistance [ohms] for wye connections. Set less than zero ungrounded wye."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(14), ctypes.c_double(0)))
 
@@ -95,7 +95,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(15), ctypes.c_double(argument)))
 
-    def _xhl(self) -> float:
+    def _xhl_read(self) -> float:
         """Gets the percent reactance between windings 1 and 2, on winding 1 kVA base. Use for 2 winding or 3 winding
         transformers. """
         return float(self.dss_obj.TransformersF(ctypes.c_int32(16), ctypes.c_double(0)))
@@ -106,7 +106,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(17), ctypes.c_double(argument)))
 
-    def _xht(self) -> float:
+    def _xht_read(self) -> float:
         """Gets the percent reactance between windings 1 and 3, on winding 1 kVA base. Use for 3 winding transformers
         only."""
         return float(self.dss_obj.TransformersF(ctypes.c_int32(18), ctypes.c_double(0)))
@@ -117,7 +117,7 @@ class TransformersF(Base):
         argument = Base.check_float_param(argument)
         return float(self.dss_obj.TransformersF(ctypes.c_int32(19), ctypes.c_double(argument)))
 
-    def _xlt(self) -> float:
+    def _xlt_read(self) -> float:
         """Gets he percent reactance between windings 2 and 3, on winding 1 kVA base. Use for 3 winding transformers
         only. """
         return float(self.dss_obj.TransformersF(ctypes.c_int32(20), ctypes.c_double(0)))
