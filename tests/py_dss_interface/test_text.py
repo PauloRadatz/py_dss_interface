e5e15481a9b78517242defe81812c5667aa6b649
PauloRadatz <pradatz@epri.com>
2022-11-17 17:12:26 -0500

    Text

--- tests/py_dss_interface/test_text.py
+++ tests/py_dss_interface/test_text.py
@@ -13,7 +13,7 @@ class TestText13Bus:
     @pytest.fixture(scope='function')
     def dss(self, solve_snap_13bus):
         dss = solve_snap_13bus
-        dss.solution.solve
+        dss.solution.solve()
 
         return dss
 
