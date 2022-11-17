f1f8af0e39617bf55291d28385f37bc81b751317
PauloRadatz <pradatz@epri.com>
2022-11-17 12:02:13 -0500

    Monitors

--- src/py_dss_interface/models/Monitors/Monitors.py
+++ src/py_dss_interface/models/Monitors/Monitors.py
@@ -5,11 +5,125 @@
 from py_dss_interface.models.Monitors.MonitorsI import MonitorsI
 from py_dss_interface.models.Monitors.MonitorsS import MonitorsS
 from py_dss_interface.models.Monitors.MonitorsV import MonitorsV
-
+from typing import List
 
 class Monitors(MonitorsI, MonitorsV, MonitorsS):
     """
     This interface implements the Monitors (IMonitors) interface of OpenDSS by declaring 3 procedures for accessing
-    the different properties included in this interface: MonitorsI, MonitorsV, MonitorsS.
+    the different properties included in this interface: MonitorsI._, MonitorsV, MonitorsS.
     """
-    pass
+
+    def __init__(self, obj_dss):
+        super().__init__(obj_dss)
+
+    def first(self) -> int:
+        return MonitorsI._first(self)
+
+    def next(self) -> int:
+        return MonitorsI._next(self)
+
+    def reset(self) -> int:
+        return MonitorsI._reset(self)
+
+    def reset_all(self) -> int:
+        return MonitorsI._reset_all(self)
+
+    def sample(self) -> int:
+        return MonitorsI._sample(self)
+
+    def save(self) -> int:
+        return MonitorsI._save(self)
+
+    def show(self) -> int:
+        return MonitorsI._show(self)
+
+    @property
+    def mode(self) -> int:
+        return MonitorsI._mode(self)
+
+    @mode.setter
+    def mode(self, argument):
+        MonitorsI._mode_write(self, argument)
+
+    @property
+    def sample_count(self) -> int:
+        return MonitorsI._sample_count(self)
+
+    def sample_all(self) -> int:
+        return MonitorsI._sample_all(self)
+
+    def save_all(self) -> int:
+        return MonitorsI._save_all(self)
+
+    @property
+    def count(self) -> int:
+        return MonitorsI._count(self)
+
+    def process(self) -> int:
+        return MonitorsI._process(self)
+
+    def process_all(self) -> int:
+        return MonitorsI._process_all(self)
+
+    @property
+    def file_version(self) -> int:
+        return MonitorsI._file_version(self)
+
+    @property
+    def record_size(self) -> int:
+        return MonitorsI._record_size(self)
+
+    @property
+    def num_channels(self) -> int:
+        return MonitorsI._num_channels(self)
+
+    @property
+    def terminal(self) -> int:
+        return MonitorsI._terminal(self)
+
+    @terminal.setter
+    def terminal(self, argument: int):
+        MonitorsI._terminal_write(self, argument)
+
+    @property
+    def file_name(self) -> str:
+        return MonitorsS._file_name(self)
+
+    @property
+    def name(self) -> str:
+        return MonitorsS._name_read(self)
+
+    @name.setter
+    def name(self, argument: str):
+        MonitorsS._name_write(self, argument)
+
+    @property
+    def element(self) -> str:
+        return MonitorsS._element_read(self)
+
+    @element.setter
+    def element(self, argument: str):
+        MonitorsS._element_write(self, argument)
+
+    @property
+    def names(self) -> List[str]:
+        return MonitorsV._names(self)
+
+    @property
+    def byte_stream(self) -> List[str]:
+        return MonitorsV._byte_stream(self)
+
+    @property
+    def header(self) -> List[str]:
+        return MonitorsV._header(self)
+
+    @property
+    def dbl_hour(self) -> List[str]:
+        return MonitorsV._dbl_hour(self)
+
+    @property
+    def dbl_freq(self) -> List[str]:
+        return MonitorsV._dbl_freq(self)
+
+    def channel(self, argument) -> List[str]:
+        return MonitorsV._channel(self, argument)
--- src/py_dss_interface/models/Monitors/MonitorsV.py
+++ src/py_dss_interface/models/Monitors/MonitorsV.py
@@ -6,7 +6,7 @@ import ctypes
 import numpy as np
 from py_dss_interface.models import Bridge
 from py_dss_interface.models.Base import Base
-
+from typing import List
 
 class MonitorsV(Base):
     """
@@ -17,30 +17,31 @@ class MonitorsV(Base):
     one of the following.
     """
 
-    def _all_names(self):
+    def _names(self) -> List[str]:
         """Returns an array of all Monitor names (array of strings)."""
         return Bridge.var_array_function(self.dss_obj.MonitorsV, ctypes.c_int(0), ctypes.c_int(0), None)
 
-    def _byte_stream(self):
+    def _byte_stream(self) -> List[str]:
         """Returns a byte array containing monitor stream values. Make sure a "save" is done first (standard solution
         modes do this automatically). """
         return Bridge.var_array_function(self.dss_obj.MonitorsV, ctypes.c_int(1), ctypes.c_int(0), None)
 
-    def _header(self):
+    def _header(self) -> List[str]:
         """Returns the header string; Variant array of strings containing Channel Names."""
         return Bridge.var_array_function(self.dss_obj.MonitorsV, ctypes.c_int(2), ctypes.c_int(0), None)
 
-    def _dbl_hour(self):
+    def _dbl_hour(self) -> List[str]:
         """Returns returns a variant array of doubles containing time value in hours for the time-sampled monitor
         values; empty if frequency-sampled values for harmonics solution (see dblFreq)."""
         return Bridge.var_array_function(self.dss_obj.MonitorsV, ctypes.c_int(3), ctypes.c_int(0), None)
 
-    def _dbl_freq(self):
+    def _dbl_freq(self) -> List[str]:
         """Returns a variant array of doubles containing time values for harmonics mode solutions; empty for time
         mode solutions (use dblHour). """
         return Bridge.var_array_function(self.dss_obj.MonitorsV, ctypes.c_int(4), ctypes.c_int(0), None)
 
-    def _channel_write(self, argument) -> str:
+    # todo check type of argument
+    def _channel(self, argument) -> List[str]:
         """Returns a variant array of doubles for the specified channel (usage: MyArray = DSSmonitor. Channel(i)) A
         save or SaveAll should be executed first. Done automatically by most standard solution modes. """
         r = np.array(self._byte_stream())
