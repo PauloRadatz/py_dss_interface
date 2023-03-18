912c93536f2e50572d5f0875d18d45cd44045e00
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 12:40:48 -0400

    Reclosers docstring

--- src/py_dss_interface/models/Reclosers/ReclosersI.py
+++ src/py_dss_interface/models/Reclosers/ReclosersI.py
@@ -17,61 +17,46 @@ class ReclosersI(Base):
     """
 
     def _count(self) -> int:
-        """Gets number of Reclosers in active circuit."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(0), ctypes.c_int32(0))
 
     def _first(self) -> int:
-        """Sets first recloser to be active Circuit Element. Returns 0 if none."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(1), ctypes.c_int32(0))
 
     def _next(self) -> int:
-        """Sets next recloser to be active Circuit Element. Returns 0 if none."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(2), ctypes.c_int32(0))
 
     def _monitored_term_read(self) -> int:
-        """Gets the terminal number of Monitored Object for the Recloser."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(3), ctypes.c_int32(0))
 
     def _monitored_term_write(self, argument) -> int:
-        """Sets the terminal number of Monitored Object for the Recloser."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(4), ctypes.c_int32(argument))
 
     def _switched_term_read(self) -> int:
-        """Gets the terminal of the controlled device being switched by the Recloser."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(5), ctypes.c_int32(0))
 
     def _switched_term_write(self, argument) -> int:
-        """Sets the terminal of the controlled device being switched by the Recloser."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(6), ctypes.c_int32(argument))
 
     def _num_fast_read(self) -> int:
-        """Gets the number of fast shots."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(7), ctypes.c_int32(0))
 
     def _num_fast_write(self, argument) -> int:
-        """Sets the number of fast shots."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(8), ctypes.c_int32(argument))
 
     def _shots_read(self) -> int:
-        """Gets the number of shots to lockout (fast + delayed)."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(9), ctypes.c_int32(0))
 
     def _shots_write(self, argument) -> int:
-        """Sets the number of shots to lockout (fast + delayed)."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(10), ctypes.c_int32(argument))
 
     def _open(self) -> int:
-        """Open recloser's controlled element and lock out the recloser."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(11), ctypes.c_int32(0))
 
     def _close(self) -> int:
-        """Close the switched object controlled by the recloser. Resets recloser to first operation."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(12), ctypes.c_int32(0))
 
     def _idx_read(self) -> int:
-        """Gets the active recloser by index into the recloser list. 1..Count."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(13), ctypes.c_int32(0))
 
     def _idx_write(self, argument) -> int:
-        """Sets the active recloser by index into the recloser list. 1..Count."""
         return self._dss_obj.ReclosersI(ctypes.c_int32(14), ctypes.c_int32(argument))
