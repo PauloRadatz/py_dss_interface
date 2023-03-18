5cfc5d30ebe95a4bc5914b1f9a1981c5385e235a
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 14:26:55 -0400

    Solution docstring

--- src/py_dss_interface/models/Solution/SolutionI.py
+++ src/py_dss_interface/models/Solution/SolutionI.py
@@ -19,228 +19,163 @@ class SolutionI(Base):
     """
 
     def _solve(self) -> int:
-        """Solution for the present solution mode. Returns 0. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(0), ctypes.c_int32(0)))
 
     def _mode_read(self) -> int:
-        """Returns the present solution mode (See DSS help). """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(1), ctypes.c_int32(0)))
 
     def _mode_write(self, argument) -> int:
-        """Modifies the present solution mode (See DSS help). """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(2), ctypes.c_int32(argument)))
 
     def _hour_read(self) -> int:
-        """Returns the present hour (See DSS help). """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(3), ctypes.c_int32(0)))
 
     def _hour_write(self, argument) -> int:
-        """Modifies the present hour (See DSS help). """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(4), ctypes.c_int32(argument)))
 
     def _year_read(self) -> int:
-        """Returns the present Year (See DSS help). """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(5), ctypes.c_int32(0)))
 
     def _year_write(self, argument) -> int:
-        """Modifies the present Year (See DSS help). """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(6), ctypes.c_int32(argument)))
 
     def _iterations(self) -> int:
-        """Returns the number of iterations taken for the last solution. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(7), ctypes.c_int32(0)))
 
     def _max_iterations_read(self) -> int:
-        """Returns the Maximum number of iterations used to solve the circuit. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(8), ctypes.c_int32(0)))
 
     def _max_iterations_write(self, argument) -> int:
-        """Modifies the Maximum number of iterations used to solve the circuit. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(9), ctypes.c_int32(argument)))
 
     def _number_read(self) -> int:
-        """Returns the number of solutions to perform for MonteCarlo and time series simulations. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(10), ctypes.c_int32(0)))
 
     def _number_write(self, argument) -> int:
-        """Modifies the number of solutions to perform for MonteCarlo and time series simulations. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(11), ctypes.c_int32(argument)))
 
     def _random_read(self) -> int:
-        """Returns the randomization mode for random variables "Gaussian" o "Uniform". """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(12), ctypes.c_int32(0)))
 
     def _random_write(self, argument) -> int:
-        """Modifies the randomization mode for random variables "Gaussian" o "Uniform". """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(13), ctypes.c_int32(argument)))
 
     def _load_model_read(self) -> int:
-        """Returns the Load Model: {dssPowerFlow (default)|dssAdmittance}. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(14), ctypes.c_int32(0)))
 
     def _load_model_write(self, argument) -> int:
-        """Modifies the Load Model: {dssPowerFlow (default)|dssAdmittance}. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(15), ctypes.c_int32(argument)))
 
     def _add_type_read(self) -> int:
-        """Returns the type of device to add in AutoAdd Mode: {dssGen (default)|dssCap}. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(16), ctypes.c_int32(0)))
 
     def _add_type_write(self, argument) -> int:
-        """Modifies the type of device to add in AutoAdd Mode: {dssGen (default)|dssCap}. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(17), ctypes.c_int32(argument)))
 
     def _algorithm_read(self) -> int:
-        """Returns the base solution algorithm: {dssNormalSolve | dssNewtonSolve}. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(18), ctypes.c_int32(0)))
 
     def _algorithm_write(self, argument) -> int:
-        """Modifies the base solution algorithm: {dssNormalSolve | dssNewtonSolve}. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(19), ctypes.c_int32(argument)))
 
     def _control_mode_read(self) -> int:
-        """Returns the mode for control devices: {dssStatic (default) | dssEvent | dssTime}. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(20), ctypes.c_int32(0)))
 
     def _control_mode_write(self, argument) -> int:
-        """Modifies the mode for control devices: {dssStatic (default) | dssEvent | dssTime}. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(21), ctypes.c_int32(argument)))
 
     def _control_iterations_read(self) -> int:
-        """Returns the current value of the control iteration counter. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(22), ctypes.c_int32(0)))
 
     def _control_iterations_write(self, argument) -> int:
-        """Modifies the current value of the control iteration counter. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(23), ctypes.c_int32(argument)))
 
     def _max_control_iterations_read(self) -> int:
-        """Returns the maximum allowable control iterations."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(24), ctypes.c_int32(0)))
 
     def _max_control_iterations_write(self, argument) -> int:
-        """Modifies the maximum allowable control iterations. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(25), ctypes.c_int32(argument)))
 
     def _sample_do_control_actions(self) -> int:
-        """Sample controls and then process the control queue for present control mode and dispatch control actions.
-        Returns 0."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(26), ctypes.c_int32(0)))
 
     def _check_fault_status(self) -> int:
-        """Executes status check on all fault objects defined in the circuit. Returns 0."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(27), ctypes.c_int32(0)))
 
     def _solve_direct(self) -> int:
-        """Executes a direct solution from the system Y matrix, ignoring compensation currents of loads, generators
-        (includes Yprim only)."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(28), ctypes.c_int32(0)))
 
     def _solve_power_flow(self) -> int:
-        """Solves using present power flow method. Iterative solution rather than direct solution. """
         return int(self._dss_obj.SolutionI(ctypes.c_int32(29), ctypes.c_int32(0)))
 
     def _solve_no_control(self) -> int:
-        """Is similar to SolveSnap except no control actions are checked or executed."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(30), ctypes.c_int32(0)))
 
     def _solve_plus_control(self) -> int:
-        """Executes a power flow solution (SolveNoControl) plus executes a CheckControlActions that executes any
-        pending control actions."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(31), ctypes.c_int32(0)))
 
     def _init_snap(self) -> int:
-        """Initializes some variables for snap shot power flow. SolveSnap does this automatically."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(32), ctypes.c_int32(0)))
 
     def _check_controls(self) -> int:
-        """Performs the normal process for sampling and executing Control Actions and Fault Status and rebuilds Y
-        if necessary."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(33), ctypes.c_int32(0)))
 
     def _sample_control_devices(self) -> int:
-        """Executes a sampling of all intrinsic control devices, which push control actions into the control queue."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(34), ctypes.c_int32(0)))
 
     def _do_control_actions(self) -> int:
-        """Pops control actions off the control queue and dispatches to the proper control element."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(35), ctypes.c_int32(0)))
 
     def _build_y_matrix(self) -> int:
-        """Forces building of the System Y matrix according to the argument:
-        {1= series elements only | 2= Whole Y matrix}."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(36), ctypes.c_int32(0)))
 
     def _system_y_changed(self) -> int:
-        """Indicates if elements of the System Y have been changed by recent activity. If changed returns 1;
-        otherwise 0."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(37), ctypes.c_int32(0)))
 
     def _converged_read(self) -> int:
-        """Indicates whether the circuit solution converged (1 converged | 0 not converged)."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(38), ctypes.c_int32(0)))
 
     def _converged_write(self, argument) -> int:
-        """Modifies the converged flag (1 converged | 0 not converged). """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(39), ctypes.c_int32(argument)))
 
     def _total_iterations(self) -> int:
-        """Returns the total iterations including control iterations for most recent solution."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(40), ctypes.c_int32(0)))
 
     def _most_iterations_done(self) -> int:
-        """Returns the max number of iterations required to converge at any control iteration of the most recent
-        solution.."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(41), ctypes.c_int32(0)))
 
     def _control_actions_done_read(self) -> int:
-        """Indicates that the control actions are done: {1 done, 0 not done}."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(42), ctypes.c_int32(0)))
 
     def _control_actions_done_write(self, argument) -> int:
-        """Modifies the flag to indicate that the control actions are done: {1 done, 0 not done}. """
         argument = Base._check_int_param(argument)
         return int(self._dss_obj.SolutionI(ctypes.c_int32(43), ctypes.c_int32(argument)))
 
     def _finish_time_step(self) -> int:
-        """Calls cleanup, sample monitors, and increment time at end of time step."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(44), ctypes.c_int32(0)))
 
     def _clean_up(self) -> int:
-        """Update storage, invcontrol, etc., at end of time step."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(45), ctypes.c_int32(0)))
 
     def _solve_all(self) -> int:
-        """Starts the solution process for all the actors created in memory.
-        Please be sure that the circuits of each actor have been compiled and ready to be solved before using this
-        command."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(46), ctypes.c_int32(0)))
 
     def _calc_inc_matrix(self) -> int:
-        """Starts the calculation of the incidence matrix for the active actor.
-        Please be sure that the circuits of each actor have been compiled and ready to be solved before using this
-        command."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(47), ctypes.c_int32(0)))
 
     # TODO include in test
     def _calc_inc_matrix_0(self) -> int:
-        """Starts the calculation of the Branch to Node incidence matrix for the active actor.
-        Please be sure that the circuits of each actor have been compiled and ready to be solved before
-        using this command. The difference between this command and the CalcIncMatrix is that the calculated matrix
-        will be ordered hierarchically from the substation to the feeder end, which can be helpful for many operations.
-        Additionally, the Bus Levels vector is calculated and the rows (PDElements) and columns (Buses) are permuted so
-        it is easy to identify their position in the circuit."""
         return int(self._dss_obj.SolutionI(ctypes.c_int32(48), ctypes.c_int32(0)))
--- src/py_dss_interface/models/Solution/SolutionV.py
+++ src/py_dss_interface/models/Solution/SolutionV.py
@@ -22,37 +22,20 @@ class SolutionV(Base):
     """
 
     def _event_log(self) -> List[str]:
-        """Returns an array of strings containing the Event Log."""
         Error.method_not_working("getter of dss.solution.event_log")
         # return Bridge.variant_pointer_read(self.dss_obj.SolutionV, 0)
 
     def _nc_matrix(self) -> List[int]:
-        """Returns an array of integers containing the incidence matrix (1-D). Each cell of the incidence matrix is
-        delivered using 3 elements of the array delivered, the first is the row, the second is the column and the
-        third is the value (1/-1). This procedure will only deliver the non-zero elements.. """
         return Bridge.variant_pointer_read(self._dss_obj.SolutionV, 1)
 
     def _bus_levels(self) -> List[int]:
-        """Returns an array of integers containing BusLevels array. This array gives a numeric value to each bus to
-        specify how far it is from the circuit?s backbone (a continuous path from the feeder head to the feeder end).
-        It is very handy to understand the circuit?s topology. """
         return Bridge.variant_pointer_read(self._dss_obj.SolutionV, 2)
 
     def _inc_matrix_rows(self) -> List[str]:
-        """Returns an array of strings specifying the way the rows of the incidence matrix (PDElements) are organized,
-         depending on the way the Branch to node incidence matrix was calculated (CalcIncMatrix/CalcIncMatrix_O)
-         the result could be very different.."""
         return Bridge.variant_pointer_read(self._dss_obj.SolutionV, 3)
 
     def _inc_matrix_cols(self) -> List[str]:
-        """Returns an array of strings specifying the way the cols of the incidence matrix (buses) are organized,
-        depending on the way the Branch to node incidence matrix was calculated (CalcIncMatrix/CalcIncMatrix_O)
-        the result could be very different."""
         return Bridge.variant_pointer_read(self._dss_obj.SolutionV, 4)
 
     def _laplacian(self) -> List[int]:
-        """Returns an array of integers containing the Laplacian matrix using the incidence matrix previously calculated
-        , this means that before calling this command the incidence matrix needs to be calculated using
-        calcincmatrix/calcincmatrix_o. This command will return only the non-zero values in compressed coordinate
-        format (row, col, value).."""
         return Bridge.variant_pointer_read(self._dss_obj.SolutionV, 5)
