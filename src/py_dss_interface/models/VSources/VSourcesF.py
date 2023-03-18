7548e0137dfff78a66f79ce46a62663abc12ff31
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 14:55:48 -0400

    VSources docstring

--- src/py_dss_interface/models/VSources/VSourcesF.py
+++ src/py_dss_interface/models/VSources/VSourcesF.py
@@ -19,37 +19,29 @@ class VSourcesF(Base):
     """
 
     def _base_kv_read(self) -> float:
-        """Gets the source voltage in kV."""
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(0), ctypes.c_double(0)))
 
     def _base_kv_write(self, argument) -> float:
-        """Sets the source voltage in kV."""
         argument = Base._check_float_param(argument)
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(1), ctypes.c_double(argument)))
 
     def _pu_read(self) -> float:
-        """Gets the source voltage in pu."""
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(2), ctypes.c_double(0)))
 
     def _pu_write(self, argument) -> float:
-        """Sets the source voltage in pu."""
         argument = Base._check_float_param(argument)
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(3), ctypes.c_double(argument)))
 
     def _angle_deg_read(self) -> float:
-        """Gets the source phase angle of first phase in degrees."""
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(4), ctypes.c_double(0)))
 
     def _angle_deg_write(self, argument) -> float:
-        """Sets the source phase angle of first phase in degrees."""
         argument = Base._check_float_param(argument)
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(5), ctypes.c_double(argument)))
 
     def _frequency_read(self) -> float:
-        """Gets the source frequency in Hz."""
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(6), ctypes.c_double(0)))
 
     def _frequency_write(self, argument) -> float:
-        """Sets the source frequency in Hz."""
         argument = Base._check_float_param(argument)
         return float(self._dss_obj.VsourcesF(ctypes.c_int32(7), ctypes.c_double(argument)))
