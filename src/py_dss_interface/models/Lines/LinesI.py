807d2bc1195ca790399ecfe416bc0b8d4e0f49b2
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 11:02:08 -0400

    lines docstrings

--- src/py_dss_interface/models/Lines/LinesI.py
+++ src/py_dss_interface/models/Lines/LinesI.py
@@ -22,50 +22,29 @@ class LinesI(Base):
     """
 
     def _first(self) -> int:
-        """Sets the first element active. Returns 0 if no lines. Otherwise, index of the line element."""
         return self._dss_obj.LinesI(ctypes.c_int32(0), ctypes.c_int32(0))
 
     def _next(self) -> int:
-        """Sets the next element active. Returns 0 if no lines. Otherwise, index of the line element."""
         return self._dss_obj.LinesI(ctypes.c_int32(1), ctypes.c_int32(0))
 
     def _phases_read(self) -> int:
-        """Gets the number of phases of the active line object."""
         return self._dss_obj.LinesI(ctypes.c_int32(2), ctypes.c_int32(0))
 
     def _phases_write(self, argument: int) -> int:
-        """Sets the number of phases of the active line object."""
         argument = Base._check_int_param(argument)
         return self._dss_obj.LinesI(ctypes.c_int32(3), ctypes.c_int32(argument))
 
     def _num_cust(self) -> int:
-        """Gets the number of customers on this line section."""
         return self._dss_obj.LinesI(ctypes.c_int32(4), ctypes.c_int32(0))
 
     def _parent(self) -> int:
-        """Gets the parents of the active Line to be the active Line. Return 0 if no parent or action fails."""
         return self._dss_obj.LinesI(ctypes.c_int32(5), ctypes.c_int32(0))
 
     def _count(self) -> int:
-        """Gets the number of Line Objects in Active Circuit."""
         return self._dss_obj.LinesI(ctypes.c_int32(6), ctypes.c_int32(0))
 
     def _units_read(self) -> int:
-        """Gets the units of the line (distance, check manual for details)."""
         return self._dss_obj.LinesI(ctypes.c_int32(7), ctypes.c_int32(0))
 
     def _units_write(self, argument: int) -> int:
-        """Sets the units of the line (distance, check manual for details).
-        units: {none | mi|kft|km|m|Ft|in|cm }
-        UNITS_MAXNUM =9;
-        UNITS_NONE   =0;
-        UNITS_MILES =1;
-        UNITS_KFT   =2;
-        UNITS_KM    =3;
-        UNITS_M     =4;
-        UNITS_FT    =5;
-        UNITS_IN    =6;
-        UNITS_CM    =7;
-        UNITS_MM    =8;
-        """
         return self._dss_obj.LinesI(ctypes.c_int32(8), ctypes.c_int32(argument))
--- src/py_dss_interface/models/Lines/LinesS.py
+++ src/py_dss_interface/models/Lines/LinesS.py
@@ -22,63 +22,51 @@ class LinesS(Base):
     """
 
     def _name_read(self) -> str:
-        """Gets the name of the active Line element."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(0), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _name_write(self, argument: str) -> str:
-        """Sets the name of the Line element to set it active."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(1), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _bus1_read(self) -> str:
-        """Gets the name of bus for terminal 1."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(2), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _bus1_write(self, argument: str) -> str:
-        """Sets the name of bus for terminal 1."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(3), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _bus2_read(self) -> str:
-        """Gets the name of bus for terminal 2."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(4), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _bus2_write(self, argument: str) -> str:
-        """Sets the name of bus for terminal 2."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(5), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     # TODO: check conflict with another method in this class. Are they the same?
     def _linecode_read(self) -> str:
-        """Gets the name of LineCode object that defines the impedances."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(6), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     # TODO: check conflict with another method in this class. Are they the same?
     def _linecode_write(self, argument: str) -> str:
-        """Sets the name of LineCode object that defines the impedances."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(7), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _geometry_read(self) -> str:
-        """Gets the name of the Line geometry code."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(8), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _geometry_write(self, argument: str) -> str:
-        """Sets the name of the Line geometry code."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(9), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _spacing_read(self) -> str:
-        """Gets the name of the Line spacing code."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(10), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _spacing_write(self, argument: str) -> str:
-        """Sets the name of the Line spacing code."""
         result = ctypes.c_char_p(self._dss_obj.LinesS(ctypes.c_int32(11), argument.encode('ascii')))
         return result.value.decode('ascii')
