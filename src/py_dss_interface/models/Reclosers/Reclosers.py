2047a576f6f2ee0a8dfe7dac3fd2b55c7fd6fdf4
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 12:40:48 -0400

    Reclosers docstring

--- src/py_dss_interface/models/Reclosers/Reclosers.py
+++ src/py_dss_interface/models/Reclosers/Reclosers.py
@@ -18,6 +18,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def phase_trip(self) -> float:
+        """Gets the phase trip curve multiplier or actual amps.
+        Sets the phase trip curve multiplier or actual amps."""
         return ReclosersF._phase_trip_read(self)
 
     @phase_trip.setter
@@ -26,6 +28,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def phase_inst(self) -> float:
+        """Gets the phase instantaneous curve multiplier or actual amps.
+        Sets the phase instantaneous curve multiplier or actual amps."""
         return ReclosersF._phase_inst_read(self)
 
     @phase_inst.setter
@@ -34,6 +38,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def ground_trip(self):
+        """Gets the ground (3I0) trip multiplier or actual amps.
+        Sets the ground (3I0) trip multiplier or actual amps."""
         return ReclosersF._ground_trip_read(self)
 
     @ground_trip.setter
@@ -42,6 +48,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def ground_inst(self) -> float:
+        """Gets the ground (3I0) instantaneous trip setting - curve multiplier or actual amps.
+        Sets the ground (3I0) instantaneous trip setting - curve multiplier or actual amps."""
         return ReclosersF._ground_inst_read(self)
 
     @ground_inst.setter
@@ -50,16 +58,21 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def count(self) -> int:
+        """Gets number of Reclosers in active circuit."""
         return ReclosersI._count(self)
 
     def first(self) -> int:
+        """Sets first recloser to be active Circuit Element. Returns 0 if none."""
         return ReclosersI._first(self)
 
     def next(self) -> int:
+        """Sets next recloser to be active Circuit Element. Returns 0 if none."""
         return ReclosersI._next(self)
 
     @property
     def monitored_term(self) -> int:
+        """Gets the terminal number of Monitored Object for the Recloser.
+        Sets the terminal number of Monitored Object for the Recloser."""
         return ReclosersI._monitored_term_read(self)
 
     @monitored_term.setter
@@ -68,6 +81,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def switched_term(self) -> int:
+        """Gets the terminal of the controlled device being switched by the Recloser.
+        Sets the terminal of the controlled device being switched by the Recloser."""
         return ReclosersI._switched_term_read(self)
 
     @switched_term.setter
@@ -76,6 +91,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def num_fast(self) -> int:
+        """Gets the number of fast shots.
+        Sets the number of fast shots."""
         return ReclosersI._num_fast_read(self)
 
     @num_fast.setter
@@ -84,6 +101,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def shots(self) -> int:
+        """Gets the number of shots to lockout (fast + delayed).
+        Sets the number of shots to lockout (fast + delayed)."""
         return ReclosersI._shots_read(self)
 
     @shots.setter
@@ -91,13 +110,17 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
         ReclosersI._shots_write(self, argument)
 
     def open(self) -> int:
+        """Open recloser's controlled element and lock out the recloser."""
         return ReclosersI._open(self)
 
     def close(self) -> int:
+        """Close the switched object controlled by the recloser. Resets recloser to first operation."""
         return ReclosersI._close(self)
 
     @property
     def idx(self) -> int:
+        """Gets the active recloser by index into the recloser list. 1..Count.
+        Sets the active recloser by index into the recloser list. 1..Count."""
         return ReclosersI._idx_read(self)
 
     @idx.setter
@@ -106,6 +129,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def name(self) -> str:
+        """Gets the name of the active Recloser Object.
+        Sets the name of the active Recloser Object."""
         return ReclosersS._name_read(self)
 
     @name.setter
@@ -114,6 +139,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def monitored_obj(self) -> str:
+        """Gets the full name of object this Recloser is monitoring.
+        Sets the full name of object this Recloser is monitoring."""
         return ReclosersS._monitored_obj_read(self)
 
     @monitored_obj.setter
@@ -122,6 +149,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def switched_obj(self) -> str:
+        """Gets the full name of the circuit element that is being switched by this Recloser.
+        Sets the full name of the circuit element that is being switched by this Recloser."""
         return ReclosersS._switched_obj_read(self)
 
     @switched_obj.setter
@@ -130,6 +159,8 @@ class Reclosers(ReclosersI, ReclosersV, ReclosersS, ReclosersF):
 
     @property
     def names(self) -> List[str]:
+        """Gets a variant array of strings with names of all reclosers in active circuit.
+        Gets a variant array of doubles: reclose intervals (s) between shots."""
         return ReclosersV._names(self)
 
     @property
