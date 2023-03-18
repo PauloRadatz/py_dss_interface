c8411f7669a160e242b4b97f7e8efb0e35f98842
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 09:42:41 -0400

    cktelement docstring

--- src/py_dss_interface/models/CktElement/CktElementV.py
+++ src/py_dss_interface/models/CktElement/CktElementV.py
@@ -23,12 +23,9 @@ class CktElementV(Base):
     """
 
     def _bus_names(self) -> List[str]:
-        """Delivers an array of strings with the names of all the buses connected to the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 0)
 
     def _bus_names_write(self, argument: List[str]):
-        """Allows to fix an array of strings with the names of all the buses connected to the active circuit element."""
-
         # # TODO learn what to do.
         # for i in range(len(argument)):
         #     argument[i] = argument[i].encode('ascii')
@@ -37,86 +34,57 @@ class CktElementV(Base):
         # return Bridge.variant_pointer_write(self.dss_obj.CktElementV, 1, argument)
 
     def _voltages(self) -> List[float]:
-        """Delivers an array of doubles with the voltages at terminals of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 2)
 
     def _currents(self) -> List[float]:
-        """Delivers an array of doubles with the currents at terminals of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 3)
 
     def _powers(self) -> List[float]:
-        """Delivers an array of doubles with the powers at terminals of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 4)
 
     def _losses(self) -> List[float]:
-        """Delivers an array of doubles with the Losses at terminals of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 5)
 
     def _phase_losses(self) -> List[float]:
-        """Delivers an array of doubles with the Losses per phase at the terminals of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 6)
 
     def _seq_voltages(self) -> List[float]:
-        """Delivers an array of doubles with the symmetrical component voltages per phase at the terminals of the active
-         circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 7)
 
     def _seq_currents(self) -> List[float]:
-        """Delivers an array of doubles with the symmetrical component Currents per phase at the terminals of the active
-         circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 8)
 
     def _seq_powers(self) -> List[float]:
-        """Delivers an array of doubles with the symmetrical component powers per phase at the terminals of the active
-        circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 9)
 
     def _property_names(self) -> List[str]:
-        """Delivers an array of strings with the names of all the properties of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 10)
 
     def _residuals_currents(self) -> List[float]:
-        """Delivers an array of doubles with the residual currents (magnitude, angle) in all the nodes of the active
-        circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 11)
 
     def _y_prim(self) -> List[float]:
-        """Delivers an array of doubles with the Y primitive matrix (complex) of the active circuit element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 12)
 
     def _cplx_seq_voltages(self) -> List[float]:
-        """Delivers an array of doubles with the complex of sequence voltages for all terminals of the active circuit
-        element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 13)
 
     def _cplx_seq_currents(self) -> List[float]:
-        """Delivers an array of doubles with the complex of sequence currents for all terminals of the active circuit
-        element."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 14)
 
     # https://github.com/PauloRadatz/py_dss_interface/issues/3
     def _variables_names(self) -> List[str]:
-        """Delivers a Variant array of strings listing all the published state variable names, if the active circuit
-        element is a PCElement. Otherwise, null string."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 15)
 
     # https://github.com/PauloRadatz/py_dss_interface/issues/4
     def _variables_values(self) -> List[float]:
-        """Delivers a Variant array of doubles listing all the values of the state variables, if the active circuit
-        element is a PCElement. Otherwise, null string."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 16)
 
     def _node_order(self) -> List[int]:
-        """Delivers a Variant array integers variant array of integer containing the node numbers (representing phases,
-        for Example) for each conductor of each terminal."""
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 17)
 
     def _currents_mag_ang(self) -> List[float]:
-        """Delivers the currents in magnitude, angle format as a variant array of doubles of the active circuit
-        element. """
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 18)
 
     def _voltages_mag_ang(self) -> List[float]:
-        """Delivers the voltages in magnitude, angle format as a variant array of doubles of the active circuit
-        element. """
         return Bridge.variant_pointer_read(self._dss_obj.CktElementV, 19)
