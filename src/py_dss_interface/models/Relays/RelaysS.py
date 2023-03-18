e6714238963bccb73392f2219ddd66af78c251df
PauloRadatz <paulo.radatz@gmail.com>
2023-03-18 13:50:23 -0400

    Relays docstrings

--- src/py_dss_interface/models/Relays/RelaysS.py
+++ src/py_dss_interface/models/Relays/RelaysS.py
@@ -19,34 +19,28 @@ class RelaysS(Base):
     """
 
     def _name_read(self):
-        """Gets the name of the active Relay."""
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(0), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _name_write(self, argument):
-        """Sets the name of the active Relay."""
         argument = Base._check_string_param(argument)
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(1), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _monitored_obj_read(self):
-        """Gets the full name of the object this relay is monitoring."""
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(2), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _monitored_obj_write(self, argument):
-        """Sets the full name of the object this relay is monitoring."""
         argument = Base._check_string_param(argument)
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(3), argument.encode('ascii')))
         return result.value.decode('ascii')
 
     def _switched_obj_read(self):
-        """Gets the full name of element that will switched when relay trips."""
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(4), ctypes.c_int32(0)))
         return result.value.decode('ascii')
 
     def _switched_obj_write(self, argument):
-        """Sets the full name of element that will switched when relay trips."""
         argument = Base._check_string_param(argument)
         result = ctypes.c_char_p(self._dss_obj.RelaysS(ctypes.c_int32(5), argument.encode('ascii')))
         return result.value.decode('ascii')
